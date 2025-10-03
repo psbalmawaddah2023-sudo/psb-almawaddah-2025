1f9a557544c02ee1553b22aae1ab0f4d9a2152bf
Daurin Nabilatul <arnabila1410@gmail.com>
2025-10-03 15:36:02 +0700

    update fitur X

--- resources/views/superadmin/pengaturan/index.blade.php
+++ resources/views/superadmin/pengaturan/index.blade.php
@@ -3,49 +3,49 @@
 @section('title', 'Daftar Pengaturan')
 
 @section('content')
-<div class="container">
-    <h1 class="mb-4">Daftar Pengaturan</h1>
+    <div class="container">
+        <h1 class="mb-4">Daftar Pengaturan</h1>
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-    <table class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th style="width: 5%">No</th>
-                <th style="width: 25%">Nama Pengaturan</th>
-                <th style="width: 45%">Isi (Value)</th>
-                <th style="width: 15%" class="text-center">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @forelse($pengaturans as $pengaturan)
-            <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>{{ ucwords(str_replace('_', ' ', $pengaturan->key)) }}</td>
-                <td>
-                    @if(strlen($pengaturan->value) > 50)
-                        {{ Str::limit($pengaturan->value, 50) }}
-                    @else
-                        {{ $pengaturan->value ?? '-' }}
-                    @endif
-                </td>
-                <td class="text-center">
-                    <a href="{{ route('pengaturan.edit', $pengaturan->id) }}" 
-                       class="btn btn-warning btn-sm">
-                        <i class="bi bi-pencil-square"></i> Edit
-                    </a>
-                </td>
-            </tr>
-            @empty
-            <tr>
-                <td colspan="4" class="text-center">Belum ada data pengaturan</td>
-            </tr>
-            @endforelse
-        </tbody>
-    </table>
-</div>
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th style="width: 5%">No</th>
+                    <th style="width: 25%">Nama Pengaturan</th>
+                    <th style="width: 45%">Isi (Value)</th>
+                    <th style="width: 15%" class="text-center">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($pengaturans as $key => $value)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ ucwords(str_replace('_', ' ', $key)) }}</td>
+                        <td>
+                            @if (strlen($value) > 50)
+                                {{ Str::limit($value, 50) }}
+                            @else
+                                {{ $value ?? '-' }}
+                            @endif
+                        </td>
+                        <td class="text-center">
+                            <a href="{{ route('pengaturan.edit', $key) }}" class="btn btn-warning btn-sm">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="4">Tidak ada data</td>
+                    </tr>
+                @endforelse
+
+            </tbody>
+        </table>
+    </div>
 @endsection
